8776e3fdfda5152a78c101d60f5d194357e0b902
ShadowsHax <militac.smith@gmail.com>
2013-10-05 23:42:46 -0600

    Create SimTest.m
    Testing File for main simulation

---
+++ SimTest.m
@@ -0,0 +1,42 @@
+clc,clear,close all
+%{
+    Simulation Workbench
+    Currently under development: Communication Relay Simulation
+    Current Stage: Command Class Development
+%}
+maxCommsDist = 1e3;                 % Maximum Communication Distance, km
+processorSpeed = 2.4e6;             % Processor Speed, Hz
+peerCount = 4;                      % # Network Peers, excluding Central Command
+
+pathMap = logical(eye(peerCount+1));
+
+%Initialize Testing Entities
+disp('Creating Peer Network...')
+Central = CommSim.EntityData([0 0 0],0,0);
+for i=1:peerCount
+    Peers(i) = CommSim.EntityData([100*randi(10) 25+100*randi([0 10]) 100*randi(10)],0,0);
+    rng('shuffle');
+end
+disp('Peer Network Established.')
+disp('Checking Network Connections...')
+for i=1:length(Peers)
+    disp(['Checking Peer ' num2str(i) '...'])
+    if checkDistanceTo(Central, Peers(i)) < maxCommsDist
+        pathMap(i,1) = 1;
+        pathMap(1,i) = 1;
+    end
+    for j=1:length(Peers)
+        if checkDistanceTo(Peers(i),Peers(j)) < maxCommsDist
+            pathMap(i+1,j+1) = 1;
+        end
+    end
+end
+disp('Network Check Complete.')
+disp('Initializing Command Registry...')
+disp('Commands Registered with Simulation.')
+cmd = input('CommSim:>>','s');
+while ~strcmp(cmd,'exit')
+    strcmp(cmd,registry)
+    cmd = input('CommSim:>>','s');
+end
+disp('Decomposing Simulation...')
