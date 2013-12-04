a1e1c266986da5a4e4db1149e0c32d1c46dc78f6
CTE Schmitty <militac.smith@gmail.com>
2013-12-03 21:33:59 -0700

    Updated Testing file

--- +CommSim/SimTest.m
+++ +CommSim/SimTest.m
@@ -6,23 +6,25 @@ clc,clear,close all
 %}
 maxCommsDist = 1e3;                 % Maximum Communication Distance, km
 processorSpeed = 2.4e6;             % Processor Speed, Hz
-peerCount = 4;                      % # Network Peers, excluding Central Command
+peerCount = 100;                    % # Network Peers, excluding Central Command
 
 pathMap = logical(eye(peerCount+1));
 
 disp('Creating Simulation Environment...')
-environ = CommSim.Sim([0,10000,1000,0,10000],0,'Clear')
+environ = CommSim.Sim([0,10000,1000,0,10000],0,'Clear');
 disp('Creating Peer Network...')
-Central = CommSim.EntityData([0 0 0],0,[0 0 0],0);
+Central = CommSim.EntityData([0 0 0],[0 0 0],0,'Central');
 for i=1:peerCount
-    Peers(i) = CommSim.EntityData([100*randi(10) 25+100*randi([0 10]) 100*randi(10)],0,[0 0 0],0);
+    Peers(i) = CommSim.EntityData([100*randi(10) 25+100*randi([0 10]) 100*randi(10)],[10 0 0],0,['Peer' int2str(i)]);
     rng('shuffle');
 end
 environ.Entities = [Central Peers(:)'];
 disp('Peer Network Established.')
 disp('Checking Network Connections...')
+fprintf('Checking Peer ');
 for i=1:length(Peers)
-    disp(['Checking Peer ' num2str(i) '...'])
+    peerNum = [num2str(i) '...'];
+    fprintf(peerNum)
     if checkDistanceTo(Central, Peers(i)) < maxCommsDist
         pathMap(i,1) = 1;
         pathMap(1,i) = 1;
@@ -32,25 +34,15 @@ for i=1:length(Peers)
             pathMap(i+1,j+1) = 1;
         end
     end
+    if i<length(Peers)
+        fprintf(repmat('\b',1,numel(peerNum)));
+    else fprintf('\n');
+    end
 end
 disp('Network Check Complete.')
 disp('Simulation Environment Created.')
 disp('Initializing Command Registry...')
-registry = [];
-sim = CommSim.SimCMD()
-registry = registerCMD(sim,registry);
 disp('Commands Registered with Simulation.')
-cmd = input('CommSim:>>','s');
-rawCmd = cmd(1:find(cmd=='(')-1)
-while ~strcmp(rawCmd,'quitSim')
-    if ~isequal(strcmp(registry(:).CommandList(:),rawCmd),zeros(length(registry(:).CommandList(:)),1))
-        disp('Let me run that command for you...')
-        eval(['sim.' cmd])
-    else
-        error('CommSim:CmdLine', ['Command "' cmd '" not found.'])
-    end
-    cmd = input('CommSim:>>','s');
-    rawCmd = cmd(1:find(cmd=='(')-1)
-end
+sim.simStart();
 disp('Decomposing Simulation...')
-clc,clear,close all
+clc,close all
