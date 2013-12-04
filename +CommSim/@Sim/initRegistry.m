81abed39c7129be4beaf85e450a10a34824e7366
CTE Schmitty <militac.smith@gmail.com>
2013-12-03 21:38:49 -0700

    Removed MATLAB files from C++ Branch

--- +CommSim/@Sim/initRegistry.m
+++
@@ -1,6 +0,0 @@
-function Registry = initRegistry()
-%INITREGISTRY: Initialize command registry
-    Registry = [];
-    sim = CommSim.SimCMD();
-    Registry = registerCMD(sim,Registry);
-end
--- +CommSim/@SimCMD/simRestart.m
+++
@@ -1,7 +0,0 @@
-function simRestart(simEnvironment)
-%SIMRESTART: Restart simulation for Environment simEnvironment
-    simEnvironment.Time = simEnvironment.origTime;
-    simEnvironment.Weather = simEnvironment.origWeather;
-    simEnvironment.Entities = simEnvironment.origEntities;
-end
-
